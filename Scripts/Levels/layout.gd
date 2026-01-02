c095521e506f1944ca2800b16639e0fd2cf8f434
Ben <shatterwaltz@gmail.com>
2026-01-02 14:35:13 -0500

    restructure levels and layouts a bit

--- Scripts/Levels/layout.gd
+++ Scripts/Levels/layout.gd
@@ -4,6 +4,16 @@ class_name Layout
 @export var enemy_spawn_parent: Node2D
 @export var player_spawn: Node2D
 var target_enemy_count: int = 1
+var next_level: Constants.LEVELS
+
+# factory function
+static func from_data(level_data: LevelData) -> Layout:
+	# clients should only need the selected layout to load, enemies will be synced naturally
+	var selected_layout: Constants.LAYOUTS = level_data.layout_pool.pick_random()
+	var layout: Layout = load(Constants.layout_map[selected_layout]).instantiate()
+	layout.target_enemy_count = randi_range(level_data.min_enemies, level_data.max_enemies)
+	layout.next_level = level_data.next_level
+	return layout
 
 func _ready() -> void:
 	if Network.is_server:
@@ -17,5 +27,10 @@ func _ready() -> void:
 			if enemy_spawns.size() < 1:
 				break
 			var spawn_point_index: int = randi_range(0, enemy_spawns.size() - 1)
+			# TODO: randomize the enemy choice here
 			ToClientRpcs.spawn_enemy.rpc(Utils.generate_id(), Constants.ENEMIES.GOBLIN, enemy_spawns[spawn_point_index].position)
 			enemy_spawns.remove_at(spawn_point_index)
+
+func activate_room():
+	# TODO: This should uncover room, make enemies start acting, and load next level scene
+	pass
